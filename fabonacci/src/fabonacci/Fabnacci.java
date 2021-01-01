8e8f8a9a7b57f4ca10fc2b801da3647946e70755
HP <HP@192.168.1.10>
2021-01-01 16:46:47 +0530

    Fabnacci

---
+++ fabonacci/src/fabonacci/Fabnacci.java
@@ -0,0 +1,22 @@
+package fabonacci;
+
+public class Fabnacci {
+	
+	public static void main(String[] args) {
+		int a=6;
+		int b=1;
+		
+		System.out.println(a+" "+b);
+	
+	int c;
+	for(int i=1;i<=10;i++);
+			{
+				c=a+b;
+				System.out.println(""+c);
+				a=b;
+				b=c;
+			}
+}
+}
+
+
